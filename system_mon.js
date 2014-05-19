b48fd8337c7ca62e82e13e6e59802e47da5815b2
DK <dukai86@gmail.com>
2014-05-19 18:14:41 +0800

    update

--- system_mon.js
+++ system_mon.js
@@ -1,5 +1,9 @@
 var Yeelink = require('./yeelink');
 var os = require('os');
+var fs = require("fs");
+var swicher = true;
+
+
 
 function getCPUInfo(callback){ 
     var cpus = os.cpus();
@@ -31,94 +35,49 @@ function getCPUInfo(callback){
 var cpuStat1 = getCPUInfo();
 
 function getCPUUsage(){
-
-	var cpus = require('os').cpus()
-	var total = 0;
-	for(var i = 0, len = cpus.length; i < len; i++){
-		var times = cpus[i].times;
-		var percentage = 100 * (times.user + times.sys + times.irq) / (times.user + times.sys + times.irq + times.idle);
-		
-		total += percentage;
-	}
-	return (total /len).toFixed(2);
-}
-
-
-var yee = new Yeelink('4d0cd8e2e9cd21714b10696f80645d42');
-
-var swicher = true;
-
-function uploadData(){
 	var cpuStat2 = getCPUInfo();
-	
 	var idle = cpuStat2.idle - cpuStat1.idle;
 	var total = cpuStat2.total - cpuStat1.total;
-	
 	var percentage = (((total - idle) / total) * 100).toFixed(2);
-	
-	console.log(new Date);
-	yee.http({
-		path: '/v1.0/device/9007/sensor/16259/datapoints',
-		method: 'post',
-		data: JSON.stringify({
-			value: percentage
-		})
-	}, function(statusCode, body){
-		if(statusCode !== 200){
-			console.log(body);
-		}
-	});
-	
-	yee.http({
-		path: '/v1.0/device/9007/sensor/16279/datapoints',
-		method: 'post',
-		data: JSON.stringify({
-			value: ((os.totalmem() - os.freemem()) / 1024 / 1024 / 1024).toFixed(2)
-		})
-	}, function(statusCode, body){
-		if(statusCode !== 200){
-			console.log(body);
-		}
-	});
 	cpuStat1 = cpuStat2;
-	//swicher && setTimeout(uploadData, 10000);
+	return percentage;
 }
-
-//uploadData();
-
-
-setInterval(function(){
-	yee.http({
-		path: '/v1.0/device/9007/sensor/16262/datapoint'
-	}, function(statusCode, body){
-		var data = JSON.parse(body);
-		
-		if(data.value == 1){
-			swicher = true;
-		}else{
-			swicher = false;
-		}
-	});
-	
+var pdata = {}, yee = null;
+fs.readFile("config.txt", function(err, data){
+	if(err) throw err;
+	var data = JSON.parse(data);
+	pdata = data;
+	yee = new Yeelink(data.key, Yeelink.P_KEY);
 	swicher && uploadData();
-}, 10000);
+	
+	setInterval(function(){
+		yee.getLastDataPoint(pdata.device_id, pdata.sensorIds[1], function(statusCode, body){
+			var data = JSON.parse(body);
+			
+			if(data.value == 1){
+				swicher = true;
+			}else{
+				swicher = false;
+			}
+			swicher && uploadData();
+		});
+		
+	}, 10000);
+});
 
-/*
-var os = require("os"),
-    cpus = os.cpus();
-
-for(var i = 0, len = cpus.length; i < len; i++) {
-    console.log("CPU %s:", i);
-    var cpu = cpus[i], total = 0;
-    for(type in cpu.times)
-        total += cpu.times[type];
-
-    for(type in cpu.times)
-        console.log("\t", type, Math.round(100 * cpu.times[type] / total));
+function uploadData(){
+	var percentage = getCPUUsage();
+	var memoryUsage = ((os.totalmem() - os.freemem()) / 1024 / 1024 / 1024).toFixed(2);
+	console.log(percentage);
+	$("#cpu_percentage").html(percentage);
+	$("#memory_usage").html(memoryUsage);
+	var now = new Date();
+	$('#cpu_update').html(now);
+	$('#mem_update').html(now);
+	
+	yee.addDataPoint(pdata.device_id, pdata.sensorIds[0], percentage, function(){});
+	yee.addDataPoint(pdata.device_id, pdata.sensorIds[2], memoryUsage, function(){});
 }
 
 
-setInterval(function(){
-	console.log(getCPUUsage());
-}, 200);
-*/
+
