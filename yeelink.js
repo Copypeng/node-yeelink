221c0a8abe229090e143a90c18a393a8df3d6180
DK <dukai86@gmail.com>
2014-05-19 18:14:32 +0800

    complete yeelink

--- yeelink.js
+++ yeelink.js
@@ -21,11 +21,14 @@ var mix = function(base, child, deep){
     return o;
 };
 
-var Yeelink = function(accessKey){
+var Yeelink = function(accessKey, keyType){
 	this.accessKey = accessKey;
-	
+	this.keyType = keyType === undefined ? 'U-ApiKey' : keyType
 	this._initYeelink();
 };
+
+Yeelink.U_KEY = 'U-ApiKey';
+Yeelink.P_KEY = 'P-ApiKey';
 
 Yeelink.prototype = {
 	_initYeelink: function(){
@@ -37,15 +40,16 @@ Yeelink.prototype = {
      * @param callback [statusCode, body, response]
      */
 	http: function(options, callback){
-
-        var options = mix({
+		var self = this;
+		var defalutOptions = {
             hostname: 'api.yeelink.net',
             path: '/v1.0/',
             method: 'get',
-            headers: {'U-ApiKey': this.accessKey},
+            headers: (function(){var headers = {};headers[self.keyType] = self.accessKey;return headers;})(),
             encoding: 'utf8',
             data: ''
-        }, options);
+        };
+        options = mix(defalutOptions, options);
 
 		var req = http.request({
 			hostname: options.hostname,
@@ -71,6 +75,55 @@ Yeelink.prototype = {
         }
 
 		req.end();
+	},
+	/**
+	 * 根据设备ID获取信息
+	 */
+	deviceInfo: function(deviceId, callback){
+		this.http({
+			path: '/v1.0/device/' + deviceId
+		}, callback);
+	},
+	
+	sensorList: function(deviceId, callback){
+		this.http({
+			path: '/v1.0/device/' + deviceId + '/sensors'
+		}, callback);
+	},
+	
+	addDataPoint: function(deviceId, sensorId, value, callback){
+		this.http({
+			path: '/v1.0/device/' + deviceId + '/sensor/' + sensorId + '/datapoints',
+			method: 'post',
+			data: JSON.stringify({
+				value: value
+			})
+		}, callback);
+	},
+	getLastDataPoint: function(deviceId, sensorId, callback){
+		this.http({
+			path: '/v1.0/device/' + deviceId + '/sensor/' + sensorId + '/datapoint'
+		}, callback);
+	},
+	/**
+	 * 绑定一个设备
+	 */
+	bindProduct: function(productSN, userLogin, callback){
+		this.http({
+			path: '/v1.0/product/bind/' + productSN,
+			method: 'post',
+			data: JSON.stringify({
+				user_login: userLogin
+			})
+		}, callback);
+	},
+	/**
+	 * 获取产品设备信息
+	 */
+	productDeviceInfo: function(productSN, callback){
+		this.http({
+			path: '/v1.0/product/' + productSN
+		}, callback);
 	}
 }
 module.exports = Yeelink;
